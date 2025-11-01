3eb8e23ad687e9a6a6b9f4188371a4b488b37584
Shreeraj Parmar <147899890+Shreeraj-Parmar@users.noreply.github.com>
2025-11-01 21:42:03 +0530

    Update API_KEY to use a placeholder
    Replaced the actual API key with a placeholder.

--- chokoboat/js/credentials.js
+++ chokoboat/js/credentials.js
@@ -2,4 +2,4 @@
 
 export const GEMINI_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent";
 
-export const API_KEY = "AIzaSyBu0DDrT10akD5oTv9llKYmdov9_qA0_Es";
+export const API_KEY = "YOUR_API_KEY";
